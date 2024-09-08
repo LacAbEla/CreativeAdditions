99c462e3c84c35b7c86413ad6b8767bc8e6e134e
LacAbEla <47212432+LacAbEla@users.noreply.github.com>
2024-09-08 19:30:18 +0200

    Updates to README and config  - Updated README.md  - Updated mod and project config  - Changes to FireballLauncherItem.java (only comments)

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ LacAbEla's useful and weird additions to spice up your (my) creative worlds/serv
   When endermen try to remove a block they will pick up air instead.<br>
   They will also briefly catch fire as a punishment for their crimes.
 
+* ### The GhastZooka™, a fireball launcher
+  Fire! Explosions! What else do you want from me?<br>
+  ![Isn't it beautiful?](src/main/resources/assets/creativeadditions/textures/item/fireball_launcher.png)
+
+
 ## TODO:
 * ### [WIP] Handcuffs for mobs
   Prevent those pesky mobs from getting away when they do something bad!<br>
@@ -15,5 +20,6 @@ LacAbEla's useful and weird additions to spice up your (my) creative worlds/serv
   You will now be able to attach a lead to any mob (villagers, zombies, pillagers...).<br>
   This will allow you to relocate them to a better place, like jail.
 
-* ### Fireball launcher
-  Fire! Explosions! What else do you want from me?
+* ### Better fireball launcher(s)
+  Add variations or configurations that improve projectile speed or power.<br>
+  Potential Thermonuclear GhastZooka™.
--- src/main/java/com/alebal/creativeadditions/item/FireballLauncherItem.java
+++ src/main/java/com/alebal/creativeadditions/item/FireballLauncherItem.java
@@ -12,14 +12,15 @@ import net.minecraft.world.level.Level;
 import java.util.Random;
 
 public class FireballLauncherItem extends Item {
+    // TODO replace fireball_launcher's texture with a 3D model
+    // TODO Add variations or configuration options for more speed or power
+
     private int explosionPower = 1;
 
 
     public FireballLauncherItem() {
         super(new Properties().tab(CreativeModeTab.TAB_COMBAT));
     }
-    // TODO replace fireball_launcher's texture with a 3D model
-    // TODO Add variations or configuration options for more speed or power
 
 
     @Override
@@ -40,6 +41,9 @@ public class FireballLauncherItem extends Item {
             var fireball = new LargeFireball(level, player, lookAngle.x, lookAngle.y, lookAngle.z, explosionPower); // The "offset" parameters are used to calculate the fireball's direction
             fireball.setPosRaw(fireball.getX() + lookAngle.x, fireball.getY() + player.getEyeHeight() + lookAngle.y, fireball.getZ() + lookAngle.z);
             //fireball.setDeltaMovement(player.getDeltaMovement()); // TODO this should copy the player's momentum
+            //var deltaX = player.getX() - player.xOld; // always zero :/
+            //var deltaY = player.getY() - player.yOld;
+            //var deltaZ = player.getZ() - player.zOld;
             // TODO check if there is a block ahead. The shooting offset allows to shoot through blocks that are less than 1m wide
             level.addFreshEntity(fireball);
         }
